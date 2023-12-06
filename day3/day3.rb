35c3d74c41b83574e828901e5b70c4fbd8c9481f
Jo Roe <jroe@invoca.com>
2023-12-05 17:55:47 -0800

    Day 3 Solved! With nice comments? from ChatGPT

--- day3/day3.rb
+++ day3/day3.rb
@@ -1,23 +1,79 @@
 require 'aoc/grid'
 
 class Day3
+  # Initializes a new instance of the Day3 class.
+  #
+  # Parameters:
+  # - file_name: A string representing the path to the input file.
   def initialize(file_name)
     @grid = Aoc::Grid.from_file(file_name)
   end
 
+  # Calculates the solution for part 1 of the puzzle.
+  #
+  # Returns:
+  # The sum of relevant numbers that touch symbols (not '.').
   def part1
-    # find numbers that touch symbols (not .)
+    part_numbers = find_relevant_numbers(/[^.]/).map(&:to_s).map(&:to_i)
+    part_numbers.sum
+  end
+
+  # Finds relevant numbers based on a given condition.
+  #
+  # Parameters:
+  # - condition: A regular expression pattern to match against the neighbors of each region.
+  #
+  # Returns:
+  # An array of region objects that match the given condition.
+  def find_relevant_numbers(condition)
     regions = @grid.find_horizontal_regions(/\d+/)
     part_numbers = []
     regions.each do |region|
       region.neighbors.each do |neighbor|
-        if neighbor.to_s != '.'
-          part_numbers << region.to_s.to_i
+        if neighbor.to_s =~ condition
+          part_numbers << region
           break
         end
       end
     end
-    # add 'em
-    part_numbers.sum
+    part_numbers
+  end
+
+  # Calculates the solution for part 2 of the puzzle.
+  #
+  # Returns:
+  # The sum of products of numbers where a gear touches both of the numbers.
+  def part2
+    part_number_products = []
+    part_numbers = find_relevant_numbers(/\*/)
+    find_adjoining_gears(part_numbers).each do |gear, numbers|
+      if numbers.count > 1
+        part_number_products << numbers.inject(:*)
+      end
+    end
+    part_number_products.sum
+  end
+
+  # Finds the adjoining gears for each part number.
+  #
+  # Parameters:
+  # - part_numbers: An array of region objects representing the relevant part numbers.
+  #
+  # Returns:
+  # A hash where the keys are the adjoining gears and the values are arrays of part numbers.
+  def find_adjoining_gears(part_numbers)
+    gears_and_their_numbers = {}
+    part_numbers.each do |part_number|
+      part_number.neighbors.each do |neighbor|
+        if neighbor.to_s =~ /\*/
+          if gears_and_their_numbers[neighbor] && !gears_and_their_numbers[neighbor].include?(part_number.to_s.to_i)
+            gears_and_their_numbers[neighbor] << part_number.to_s.to_i
+          else
+            gears_and_their_numbers[neighbor] = [part_number.to_s.to_i]
+          end
+        end
+      end
+    end
+    gears_and_their_numbers
   end
 end
--- day3/day3_spec.rb
+++ day3/day3_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe Day3 do
       expect(day3.part1).to eq(4361)
     end
   end
+
+  context 'part 2' do
+    let(:part_numbers) { day3.find_relevant_numbers(/\*/) }
+    it 'example sum of gear ratios is 467835' do
+      expect(day3.part2).to eq(467835)
+    end
+
+    it 'finding relevant part numbers finds numbers that touch *s' do
+      expect(part_numbers.count).to eq(5)
+    end
+
+    it 'awkward test but check find_adjoining_gears too' do
+      expect(day3.find_adjoining_gears(part_numbers)).to be_a(Hash)
+    end
+  end
 end
